5443523ab4bc7ac530b9a86e300595efe36c0bcd
VoidOfMaya <mayaofthevoid@gmail.com>
2023-03-27 19:45:15 +0300

    solve exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    return word.split('').reverse().join('');
+
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// npm test reverseString.spec.js
