e1e6340e18a2c56e23d61af0f30d3cf7bd75ee66
VoidOfMaya <mayaofthevoid@gmail.com>
2023-03-27 15:50:24 +0300

    solve js-exercise 01 + 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function( string, num) {
+    let placeHolder ='';
+    let error = 'ERROR';
+    if (num < 0) {
+        return error;
+        }else{
+        for (let i = 0; i < num; i++) {
+        
+           placeHolder += string;
+        }
+        return placeHolder.toString();
+    }
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+//npm test repeatString.spec.js
