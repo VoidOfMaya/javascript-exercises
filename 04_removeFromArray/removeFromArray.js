977e28f90ccf07ac98e6552e43edadbab69a6aaf
VoidOfMaya <mayaofthevoid@gmail.com>
2023-03-29 16:35:53 +0300

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args =Array.from(arguments).slice(1);
+    return array.filter(element => !args.includes(element));
+
 
 };
 
+
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// npm test removeFromArray.spec.js
