ca2080864593640e1b94db18d6266126bdaf196b
VoidOfMaya <mayaofthevoid@gmail.com>
2023-03-30 21:12:06 +0300

    solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let convertToC = (x -32) * 5/9;
+  return Number(convertToC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let convertToF =(x * 9/5) + 32;
+  return Number(convertToF.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +13,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//npm test tempConversion.spec.js
