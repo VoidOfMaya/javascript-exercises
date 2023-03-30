1183305748674cc3a99c56d1811690c03a9a858c
VoidOfMaya <mayaofthevoid@gmail.com>
2023-03-30 20:50:13 +0300

    solve 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+// leap years:
+// devisable by 4
+// not devisable by 100 unless its by 400
+//
+//
+//
+//
+//
+//
+
+const leapYears = function(year) {
+    if(year % 4 === 0 && (year % 100  !== 0 || year % 400 === 0) ){
+        return true;
+    }
+    return false;
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//npm test leapYears.spec.js
